eaf595a9f9118afb4f13d5dfb62247e8a7a854e3
Gaurav Bhardwaj <myselfgroot@gmail.com>
2020-03-24 21:20:59 +0530

    Added local bootstrap and scss loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {AlertService} from "./app/alert.service";
 import {ComponentService} from "./app/component.service";
 import {run} from "./app/app";
-
+import "./main.scss"
 run(new AlertService(), new ComponentService());
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,22 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
+    devtool: false,
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader","css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader","css-loader"]
+            }
+        ]
     }
 }
